46b6f8d0d93b16b66b1590deda3ad1015a4dd7f1
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-09-16 14:35:57 -0500

    basic app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    cohortName: 'Tarjan',
+    salutation: 'Hello',
+    name: 'Gabriel'
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <h1>{this.state.cohortName} Learns State </h1>
+    <h2>{this.state.salutation} my name is {this.state.name}</h2>
       </div>
     );
   }

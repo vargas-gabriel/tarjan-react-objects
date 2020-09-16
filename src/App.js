0aa6d2f16ac4dbe2af6057b5c69c3257079fa257
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-09-16 14:43:32 -0500

    added some html

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ class App extends Component {
   state = {
     cohortName: 'Tarjan',
     salutation: 'Hello',
-    name: 'Gabriel'
+    name: 'Gabriel',
+    whereILive: 'Loring Park',
+    sport: 'soccer'
   }
   render() {
     return (
-      <div className="App">
+      <div>
        <h1>{this.state.cohortName} Learns State </h1>
     <h2>{this.state.salutation} my name is {this.state.name}</h2>
+    <ul>
+    <li>My name is {this.state.name}</li>
+    <li>I live in {this.state.whereILive}</li>
+    <li>I play {this.state.sport}</li>
+    </ul>
       </div>
     );
   }
